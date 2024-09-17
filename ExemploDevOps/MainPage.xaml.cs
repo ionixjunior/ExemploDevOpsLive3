e51110bd6fccf5150636692f280f798600e2c19c
Ione Souza Junior <junior@ionixjunior.com.br>
2024-09-16 23:21:20 -0300

    Alterar texto

--- ExemploDevOps/MainPage.xaml.cs
+++ ExemploDevOps/MainPage.xaml.cs
@@ -14,9 +14,9 @@ public partial class MainPage : ContentPage
         count++;
 
         if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
+            CounterBtn.Text = $"Clicado {count} vez";
         else
-            CounterBtn.Text = $"Clicked {count} times";
+            CounterBtn.Text = $"Clicado {count} vezes";
 
         SemanticScreenReader.Announce(CounterBtn.Text);
     }
